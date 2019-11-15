6239d3649f59167a571bfa8a498d72f5d1ee52cb
Simeon Tsvetanov <simeon.tsvetanov@gmail.com>
2019-11-15 13:38:07 +0200

    Added Average Example

---
+++ Average/Average.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Average
+{
+    class Average
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double counter = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if ("stop".Equals(input)) break;
+
+                sum += double.Parse(input);
+                Console.WriteLine("temporary sum: " + sum);
+                counter++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("counter: " + counter);
+            Console.WriteLine("sum: " + sum);
+            Console.WriteLine("average: " + sum / counter);
+        }
+    }
+}
